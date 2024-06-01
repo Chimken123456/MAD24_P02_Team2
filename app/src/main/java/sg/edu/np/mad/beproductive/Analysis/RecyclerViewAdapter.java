bf82acaf7d6c70b5857e0589399d9aaa8ae7912a
nicolelwn <s10260727@connect.np.edu.sg>
2024-06-02 01:20:05 +0800

    final

---
+++ app/src/main/java/sg/edu/np/mad/beproductive/Analysis/RecyclerViewAdapter.java
@@ -0,0 +1,38 @@
+package sg.edu.np.mad.beproductive.Analysis;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import sg.edu.np.mad.beproductive.R;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<AppViewHolder> {
+    ArrayList<String> data;
+    public RecyclerViewAdapter(ArrayList<String> input){
+        data = input;
+    }
+    public AppViewHolder onCreateViewHolder(
+            ViewGroup parent,
+            int viewType
+    ){
+
+        View item =LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.app_layout,
+                parent,
+                false
+        );
+        return new AppViewHolder(item);
+    }
+    public void onBindViewHolder(AppViewHolder holder, int position)
+        {
+            String s = data.get(position);
+            holder.appName.setText(s);
+        }
+    public int getItemCount(){
+        return data.size();
+    }
+}
