5be58a4e71df754d8392459a053154e5df57117e
nicolelwn <s10260727@connect.np.edu.sg>
2024-06-02 01:44:23 +0800

    analysis intent done

--- app/src/main/java/sg/edu/np/mad/beproductive/Analysis/AnalysisActivity.java
+++ app/src/main/java/sg/edu/np/mad/beproductive/Analysis/AnalysisActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.ImageView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -21,11 +20,14 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import sg.edu.np.mad.beproductive.HomePage.HomeMenu;
 import sg.edu.np.mad.beproductive.R;
+import sg.edu.np.mad.beproductive.Timetable.TimetableActivity;
 import sg.edu.np.mad.beproductive.User;
 
 
